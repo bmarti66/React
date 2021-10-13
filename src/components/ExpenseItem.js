edc1e230403500912ab281a0b26ca99bdf04d6d2
unknown <blakefitzmartin@gmail.com>
2021-10-12 23:35:10 -0500

    commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+
+	return (
+		<div>
+			<div>September, 28th 1990</div>
+			<div>
+				<h2>Car Insurance</h2>
+				<div>$295</div>
+			</div>
+		</div>
+	)
+}
+
+export default ExpenseItem;
